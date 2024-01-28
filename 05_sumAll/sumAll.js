c9e748b5bf76c92b834c40fd3e2174e2c528fd78
Yadwinder Singh <143868203+Yadsingh12@users.noreply.github.com.>
2024-01-28 16:01:01 +0530

    Complete 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    let sum = 0;
+    if(typeof(num1)!="number" || typeof(num2)!="number") return "ERROR";
+    if(num1<0 || num2<0) return "ERROR";
+    let i = num1 > num2 ? num2:num1;
+    let j = num1 > num2 ? num1:num2;
+    for ( ; i <= j; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
