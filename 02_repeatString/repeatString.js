82b3a26179df82aff85f58e31c736f97d87a90fd
Yadwinder Singh <143868203+Yadsingh12@users.noreply.github.com.>
2024-01-28 14:28:54 +0530

    Complete 2nd exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(name, times) {
+    let str="";
+    if(times<0) return "ERROR";
+    for(i=0; i<times; i++) {
+        str = str + name;
+    }
+    return str;
 };
 
 // Do not edit below this line
