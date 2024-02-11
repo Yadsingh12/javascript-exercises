ef57f8074349627b9b6532b059709f9a65c46b94
Yadwinder Singh <143868203+Yadsingh12@users.noreply.github.com.>
2024-02-11 22:46:38 +0530

    Complete 12th exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const old = people.reduce((oldest, current) => {
+    if (oldest.yearOfDeath === undefined) oldest.yearOfDeath = new Date().getFullYear();
+    if (current.yearOfDeath === undefined) current.yearOfDeath = new Date().getFullYear();
+    const lifeOldest = oldest.yearOfDeath - oldest.yearOfBirth;
+    const lifeCurrent = current.yearOfDeath - current.yearOfBirth;
+    return (lifeOldest < lifeCurrent) ? current : oldest;
+  }, people[0])
+  return old;
 };
 
 // Do not edit below this line
