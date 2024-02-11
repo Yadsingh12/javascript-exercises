6e153af7f4e0ec75048002b5871e7a3929ff3f81
Yadwinder Singh <143868203+Yadsingh12@users.noreply.github.com.>
2024-02-11 17:29:38 +0530

    Complete 09 exercise.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  string = string.toUpperCase().replace(/\W/g, '');;
+  const arr = string.split("");
+  const arr2 = arr.reverse();
+  const reversedString = arr2.join("");
+  return(string===reversedString);
 };
 
 // Do not edit below this line
