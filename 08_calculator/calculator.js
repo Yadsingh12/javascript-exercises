113eaadee56fc4c86dcd57a29050bd388a4814aa
Yadwinder Singh <143868203+Yadsingh12@users.noreply.github.com.>
2024-02-11 17:10:25 +0530

    Complete 8th exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return(a+b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return(a-b);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  //let sum=0;
+	//for(i in arr) {sum+=arr[i];}
+  //return sum;
+  return arr.reduce((sum, current) => (sum+current), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  //let mul=1;
+  //for(i in arr) {mul*=arr[i];}
+  //return mul;
+  return arr.reduce((product, current) => product*current);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	return (num===0)? 1 : num*factorial(num-1);
 };
 
 // Do not edit below this line
